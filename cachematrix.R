f0a20309d898546a8380582342de7c1a4a87911c
Abhit Patil <abhit95@gmail.com>
2015-04-18 02:23:55 +0530

    Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## pair of functions that check the cache and return the inverse of a matrix 
+## from cache if available in cache or inverts the matrix and stores it in 
+## cache for future use.
 
-## Write a short comment describing this function
 
+## makeCacheMatrix: This function that takes a matrix as its argument creates 
+## a special "matrix" object that can cache its inverse. 
+## It returns a list of functions as its objects
 makeCacheMatrix <- function(x = matrix()) {
+        nr <- nrow(x)
+        m <- matrix(nrow = nr, ncol = nr)    ##creates empty matrix
+        
+        set <- function(y = matrix()) {
+                x <<- y
+                nr2 <- nrow(y) 
+                m <<- matrix(nrow = nr2, ncol = nr2)
+        }
 
+        get <- function() x
+        getinverse <- function() m
+        setinverse <- function(inverse) m <<- inverse
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve: This function takes the list returned by makeCacheMatrix above and
+## computes the inverse of the special "matrix".If the inverse has already been 
+## calculated and the matrix has not changed the cachesolve retrieves the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()                ##takes the matrix from cache
+        if(!all(is.na(m))) {               ##checks if inverse contains values 
+          message("getting cached data")   ##and is not an empty matrix
+          return(m)
+        }
+        
+	  data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)      ##stores the inverse in cache for future use
+        m
 }
